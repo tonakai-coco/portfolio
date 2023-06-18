c3a9d16e323cff9a6fee74d0ac46136bab3e62e0
tonakai_coco <73034880+tonakai-coco@users.noreply.github.com>
2023-06-18 22:10:05 +0900

    Add main layout component with chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/main'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Layout router={router}>
+        <Component {...pageProps} key={router.route} />
+      </Layout>
+    </ChakraProvider>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-  return <div>Hello</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="lightblue" p={3} mb={5} align="center">
+        Hello, I'm an embedded software developer based in Tochigi!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Yoshinori Kashimura
+          </Heading>
+          <p>Software Developer</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
