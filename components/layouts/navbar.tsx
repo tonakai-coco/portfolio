c6ccf1981d519a8f9fe8c64b7cb6bf1ece6a2782
tonakai_coco <73034880+tonakai-coco@users.noreply.github.com>
2023-06-19 23:52:57 +0900

    Add Navbar component and dependencies

---
+++ components/layouts/navbar.tsx
@@ -0,0 +1,67 @@
+import NextLink from 'next/link'
+import Image from 'next/image'
+import {
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { HumburgerIcon } from '@chakra-ui/icons'
+
+const LinkItem = ({ href, path, children }) => {
+  const active = path === href
+  const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+  return (
+    <NextLink href={href}>
+      <Link
+        p={2}
+        bg={active ? 'glassTeal' : undefined}
+        color={active ? '#202023' : inactiveColor}
+      >
+        {children}
+      </Link>
+    </NextLink>
+  )
+}
+
+const Navbar = props => {
+  const { path } = props
+
+  return (
+    <Box
+      position="fixed"
+      as="nav"
+      w="100%"
+      bg={useColorModeValue('#ffffff40', '#20202380')}
+      style={{ backdropFilter: 'blur(10px)' }}
+      zIndex={1}
+      {...props}
+    >
+      <Container
+        display="flex"
+        p={2}
+        maxW="container.md"
+        wrap="wrap"
+        align="center"
+        justify="space-between"
+      >
+        <Flex align="center" mr={5}>
+          <Image src="/logo_light.png" width={48} height={48} />
+          <Heading as="h1" size="lg" letterSpacing={'tighter'} p={2}>
+            Yoshinori Kashimura
+          </Heading>
+        </Flex>
+      </Container>
+    </Box>
+  )
+}
+
+export default Navbar
