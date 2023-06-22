12653956c6896d6820b824ded853be785a598ae2
Yoshinori Kashimura <yoshinori-kashimura@hldc.co.jp>
2023-06-22 12:37:17 +0900

    Refactor file structure and imports

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,5 +1,5 @@
 import NextLink from 'next/link'
-import Logo from '../logo'
+import Logo from '@/components/logo'
 import {
   Container,
   Box,
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import Layout from '../components/layouts/main'
+import Layout from '@/components/layouts/main'
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
