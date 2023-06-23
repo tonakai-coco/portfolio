ef913dc72b4afe38822b35cc9042760644c9777e
tonakai_coco <73034880+tonakai-coco@users.noreply.github.com>
2023-06-23 22:20:27 +0900

    Refactor box background color on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
-import { Container, Box, Heading, Text } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="lightblue" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('lightblue', 'red')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;m an embedded software developer based in Tochigi!
       </Box>
-
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
