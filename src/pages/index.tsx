b33ffc673a807f1dee2217c08cfeb255130fda51
tonakai_coco <73034880+tonakai-coco@users.noreply.github.com>
2023-06-25 21:51:57 +0900

    Add profile picture and band equipment image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   Heading,
   Text,
+  Image,
   useColorModeValue
 } from '@chakra-ui/react'
 
@@ -18,16 +19,42 @@ const Page = () => {
       >
         Hello, I&apos;m an embedded software developer based in Tochigi!
       </Box>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
+      <Box display={{ md: 'flex' }} alignItems="center">
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/ykashimura.png"
+            alt="profile image"
+          />
+        </Box>
+        <Box flexGrow={1} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
           <Heading as="h2" variant="page-title">
             Yoshinori Kashimura
           </Heading>
           <Text>Software Developer</Text>
         </Box>
       </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box
+          flexShrink={1}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image src="/tonakai_band_equipment.jpeg" alt="tonakai equipment" />
+        </Box>
+      </Box>
     </Container>
   )
 }
-
 export default Page
